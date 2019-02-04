c14f69d4639e72df05c4a8a16d8342e701de3dfe
nivethajk <37238403+nivethajk@users.noreply.github.com>
2019-02-04 15:59:32 +0530

    Update vowels or consonant.py

--- vowels or consonant.py
+++ vowels or consonant.py
@@ -1,13 +1,7 @@
-str1 = input("Please Enter Your Own String : ")
-vowels = 0
-consonants = 0
-
-for i in str1:
-    if(i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'
-       or i == 'A' or i == 'E' or i == 'I' or i == 'O' or i == 'U'):
-        vowels = vowels + 1
-    else:
-        consonants = consonants + 1
- 
-print("Total Number of Vowels in this String = ", vowels)
-print("Total Number of Consonants in this String = ", consonants)
+str = input("Input a letter of the alphabet: ")
+ if str in ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'A', 'E'):
+	print("%s is a vowel." % str)
+elif str == 'y':
+	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+else:
+	print("%s is a consonant." % str) 
