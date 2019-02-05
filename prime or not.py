3ed1151f5b966b320cedff20cacc25c18a3db213
nivethajk <37238403+nivethajk@users.noreply.github.com>
2019-02-05 14:10:17 +0530

    Update prime or not.py

--- prime or not.py
+++ prime or not.py
@@ -1,9 +1,9 @@
-a=int(input("Enter number: "))
+a=int(input("Enter the number: "))
 k=0
 for i in range(2,a//2+1):
     if(a%i==0):
         k=k+1
 if(k<=0):
-    print("Number is prime")
+    print("The entered number is Prime Number")
 else:
-    print("Number isn't prime")
+    print("The entered number is not a Prime Number")
