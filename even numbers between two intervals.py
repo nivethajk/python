0edcc878e98c9ab8168f8a99f44bad30fd1eb765
nivethajk <37238403+nivethajk@users.noreply.github.com>
2019-02-05 14:24:25 +0530

    Update even numbers between two intervals.py

--- even numbers between two intervals.py
+++ even numbers between two intervals.py
@@ -1,5 +1,5 @@
-lower_limit = int(input("Enter the lower limit : "))
-upper_limit = int(input("Enter the upper limit : "))
+lower_limit = int(input("Enter the first number : "))
+upper_limit = int(input("Enter the second number : "))
 for i in range(lower_limit,upper_limit+1):
   if(i%2 == 0):
     print(i)
