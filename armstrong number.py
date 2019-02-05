0299e7c1ae6b9f773187b40cac6ed7dc293a69ce
nivethajk <37238403+nivethajk@users.noreply.github.com>
2019-02-05 14:45:35 +0530

    Update armstrong number.py

--- armstrong number.py
+++ armstrong number.py
@@ -1,13 +1,11 @@
-num = int(input("Enter a number: "))  
+num = int(input("Enter the number: "))  
 sum = 0  
 temp = num  
-  
-while temp > 0:  
+  while temp > 0:  
    digit = temp % 10  
    sum += digit ** 3  
    temp //= 10  
-  
-if num == sum:  
+  if num == sum:  
    print(num,"is an Armstrong number")  
 else:  
    print(num,"is not an Armstrong number")  
